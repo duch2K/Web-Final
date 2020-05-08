2578e76be976b9dc1b3cf7a5e4016f87d4b01403
dula <sarsenbek.d@gmail.com>
2020-05-08 16:53:38 +0600

    modified some files

--- config/db.php
+++ config/db.php
@@ -1,3 +1,8 @@
 <?php 
 
 $connect = new PDO('mysql:host=localhost; dbname=MushroomShop; charset: utf8', 'root', 'root');
+
+if (!$connect) {
+    echo 'Failed to connect to the DB! <br>';
+    exit();
+}
--- db/db.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-$connect = new PDO('mysql:host=localhost; dbname=MushroomShop; charset: utf8', 'root', 'root');
-
-?>
