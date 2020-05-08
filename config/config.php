1da3aeae6983f0b257eadc0649ca6ea746057b38
dula <sarsenbek.d@gmail.com>
2020-05-08 16:49:55 +0600

    added many files: db, config, elems, actions, etc

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    $config = array(
+        'title' => 'DEMAL-Tech',
+        'db' => array(
+            'server' => 'localhost',
+            'username' => 'root',
+            'password' => 'root',
+            'name' => 'demal_tech'
+        )
+    );
+
+    require "db.php";
---
+++ db/db.php
@@ -0,0 +1,3 @@
+<?php 
+
+$connect = new PDO('mysql:host=localhost; dbname=MushroomShop; charset: utf8', 'root', 'root');
